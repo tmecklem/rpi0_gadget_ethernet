1ca6dbae99c4fccf5129ca823ab5ca100d949952
Tim Mecklem <timothy@mecklem.com>
2017-04-22 10:55:01 -0400

    [WIP] update nerves_system_br to the one with 4.9 rpi0 patch

--- mix.exs
+++ mix.exs
@@ -22,7 +22,7 @@ defmodule NervesSystemRpi0.Mixfile do
 
   defp deps do
     [{:nerves, "~> 0.5", runtime: false },
-     {:nerves_system_br, "~> 0.10.0", runtime: false },
+     {:nerves_system_br, github: "tmecklem/nerves_system_br", branch: "linux-4.9", runtime: false },
      {:nerves_toolchain_armv6_rpi_linux_gnueabi, "~> 0.10.0", runtime: false}]
   end
 
